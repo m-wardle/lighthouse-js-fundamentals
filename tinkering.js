deab352dd3206025f9be866e022b0a191758fa18
Matthew Wardle <mwardle.22@gmail.com>
2019-08-15 17:53:26 +0000

    Add completed tinkering file

---
+++ tinkering.js
@@ -0,0 +1,11 @@
+function multiply(a, b) {
+  return a * b;
+};
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+if (flavor == ("vanilla" || "chocolate") && vessel == ("cone" || "bowl") && toppings = ("sprinkles" || "peanuts") {
+  console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings + ".");
+}
