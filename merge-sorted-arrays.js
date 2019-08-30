7b876f86cd95476a62eb6f07cbde62ad7d3f67b6
Matthew Wardle <mwardle.22@gmail.com>
2019-08-30 03:27:27 +0000

    Remove redundant else if statement and modifiy initial if statement to accomodate equal numbers

--- merge-sorted-arrays.js
+++ merge-sorted-arrays.js
@@ -3,10 +3,7 @@ const merge = function (arrayOne, arrayTwo) {
     let arrayThree = []
     for (let x = 0; x < arrayOne.length;) {
       for (let y = 0; y < arrayTwo.length;) {
-        if (arrayTwo[y] < arrayOne[x]) {
-          arrayThree.push(arrayTwo[y])
-          arrayTwo.shift()
-        } else if (arrayTwo[y] === arrayOne[x]) {
+        if (arrayTwo[y] <= arrayOne[x]) {
           arrayThree.push(arrayTwo[y])
           arrayTwo.shift()
         } else {
@@ -31,4 +28,4 @@ console.log(merge([ 1, 2, 2, 2 ], [1, 2, 2]), "=?", [ 1, 1, 2, 2, 2, 2, 2 ]);
 console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
 console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
 console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
-console.log(merge([ -1, 7, 8, 9 ], [-4, -3, 1, 2, 3]), "=?", [ 1, 2, 3, 7, 8, 9 ]);
+console.log(merge([ -1, 7, 8, 9 ], [-4, -3, 1, 2, 3]), "=?", [ -4, -3, -1, 1, 2, 3, 7, 8, 9 ]);
