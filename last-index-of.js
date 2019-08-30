123830e0297578d16eca3491a4991afda4701ea4
Matthew Wardle <mwardle.22@gmail.com>
2019-08-30 00:57:15 +0000

    Create function to return the index of the last occurance of a number in an array

---
+++ last-index-of.js
@@ -0,0 +1,16 @@
+const lastIndexOf = function (array, num) {
+  let i = array.length;
+  while (i > 0) {
+    i--
+    if (array[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
